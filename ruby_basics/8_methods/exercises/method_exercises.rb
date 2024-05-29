c8ac30ee78a17b94d7a29c17afd267ff330e669f
macnult <113482585+macnult@users.noreply.github.com>
2024-05-28 23:24:17 -0400

    Save progress

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -6,12 +6,17 @@
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
-
+def ascii_translator(num)
+  return num.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(current_sports, favorite_sports)
+  return current_sports.intersection(favorite_sports)
+end
 
 
 # method name: #alphabetical_list
@@ -45,4 +50,3 @@
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
-
