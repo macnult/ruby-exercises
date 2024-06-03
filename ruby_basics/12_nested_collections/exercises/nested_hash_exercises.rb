80fc248ac4e9ad3d2b0f06cd9c4241fd0d52013c
macnult <113482585+macnult@users.noreply.github.com>
2024-06-03 17:15:39 -0400

    Save hash exercise project

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -7,11 +7,15 @@ def find_language_information(languages, language_name)
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
   # }
+  languages.dig(language_name)
 end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
   # hash of language_name, then return the updated languages hash
+  languages[language_name] ||= {}
+  languages[language_name][info_key] = info_value
+  languages
 end
 
 def add_language(languages, language_name, language_info_value)
